6167a083ea1eef8def23fee47ec628a766e26ec2
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-16 08:46:04 -0300

    Creating OCR

---
+++ BackEnd/WebAPI/WebAPI/Controllers/OcrController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Domains;
+using WebAPI.Utils.OCR;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrcController : ControllerBase
+    {
+        private readonly OcrService _ocrService;
+
+        public OrcController(OcrService orcService)
+        {
+            _ocrService = orcService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RecognizeText([FromForm] FileUploadModel fileUploadModel)
+        {
+            try
+            {
+                //Verifica se a imagem foi recebida
+                if (fileUploadModel == null || fileUploadModel.Image == null)
+                {
+                    return BadRequest("Nenhuma imagem foi fornecida");
+                }
+
+                //abre a conexão com o recurso
+                using (var stream = fileUploadModel.Image.OpenReadStream())
+                {
+                    //chama o método para reconhecer a  imagem
+                    var result = await _ocrService.RecognizeTextAsync(stream);
+
+                    //retorna o resultado
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Erro ao processar a imagem" + ex.Message);
+            }
+        }
+    }
+}
---
+++ BackEnd/WebAPI/WebAPI/Utils/OCR/OcrService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace WebAPI.Utils.OCR
+{
+    public class OcrService
+    {
+        private readonly string _subscriptionKey = "d349e6d90c39494da388cb98b18e1dfc";
+
+        //metodo para reconhecer o caracteres(texto) a partir de uma imagem
+        private readonly string _endpoint = "https://cvvitalhubg5dm.cognitiveservices.azure.com/";
+
+        public async Task<string> RecognizeTextAsync(Stream imageStream)
+        {
+            try
+            {
+                //cria um client para a API
+                var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials
+                    (_subscriptionKey))
+                {
+                    Endpoint = _endpoint
+                };
+
+                var ocrResult = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+                return ProcessRecognitionResult(ocrResult);
+            }
+            catch (Exception ex)
+            {
+
+                return "Erro ao reconhecer o texto" + ex.Message;
+            }
+        }
+
+        private static string ProcessRecognitionResult(OcrResult result)
+        {
+            string recognizedText = "";
+
+            //percorre todas as regioes
+            foreach (var region in result.Regions)
+            {
+                //para cada regiao percorre as linhas
+                foreach (var line in region.Lines)
+                {
+                    //para cada linha percorre as palavras
+                    foreach (var word in line.Words)
+                    {
+                        //adiciona cada palavra ao texto, separando com espaco
+                        recognizedText += word.Text + " ";
+                    }
+
+                    //quebra de linha ao final de cada linha
+                    recognizedText += "\n";
+                }
+            }
+
+            //retorna o texto
+            return recognizedText;
+        }
+    }
+}
