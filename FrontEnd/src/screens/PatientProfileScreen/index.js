3e751c6e771d002f8752ae64fa73c9bb28a44e18
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-06 10:38:22 -0300

    Fixing profile screen

--- FrontEnd/src/screens/PatientProfileScreen/index.js
+++ FrontEnd/src/screens/PatientProfileScreen/index.js
@@ -170,15 +170,16 @@ export default function PatientProfileScreen({ navigation, route }) {
               <>
                 <InternalTextArea
                   labelText="Data de nascimento:"
-                  textArea={birthDate}
+                  textArea={new Date(birthDate).toLocaleDateString()}
                   handleChangeFn={setBirthDate}
                   isEditing={isEditing}
                 />
                 <InternalTextArea
                   labelText="CPF"
-                  textArea={cpf}
+                  textArea={cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")}
                   handleChangeFn={setCpf}
-                  isEditing={isEditing}
+                  isEditing={false}
+                  keyboardType='number-pad'
                 />
               </>
             ) : (
@@ -207,7 +208,7 @@ export default function PatientProfileScreen({ navigation, route }) {
                   labelText="CRM"
                   textArea={crm}
                   handleChangeFn={setCrm}
-                  isEditing={isEditing}
+                  isEditing={false}
                 />
               </>
             )}
@@ -225,15 +226,17 @@ export default function PatientProfileScreen({ navigation, route }) {
                 widthPercentage={45}
                 handleChangeFn={setNumber}
                 isEditing={isEditing}
+                keyboardType='number-pad'
               />
             </SplitedTextAreasContainer>
             <SplitedTextAreasContainer>
               <InternalTextArea
                 widthPercentage={45}
                 labelText="Cep"
-                textArea={cep}
+                textArea={cep.replace(/(\d{5})(\d{3})/, '$1-$2')}
                 handleChangeFn={setCep}
                 isEditing={isEditing}
+                keyboardType='number-pad'
               />
               <InternalTextArea
                 widthPercentage={45}
@@ -254,10 +257,10 @@ export default function PatientProfileScreen({ navigation, route }) {
                   if (role == "Paciente") {
                     await AtualizarPerfilPaciente(
                       token,
-                      birthDate,
+                      new Date(birthDate).toISOString(),
                       neighborhood,
                       number,
-                      cep,
+                      cep.replace('-', ''),
                       city
                     );
                   } else if (role == "Medico") {
@@ -267,7 +270,7 @@ export default function PatientProfileScreen({ navigation, route }) {
                       crm,
                       neighborhood,
                       number,
-                      cep,
+                      cep.replace('-', ''),
                       city
                     );
                   }
